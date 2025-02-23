6c0855ed6ac57ae9be94687a79a90ea183edd311
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-02-23 19:32:37 +0100

    Refactor spell extraction logic to improve context logging and error handling

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/fs"
 	"log"
+	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func main() {
-	path := filepath.Join("..", "5etools-src", "data", "spells")
-	files := []string{}
+	var (
+		ctx   = context.Background()
+		path  = filepath.Join("..", "5etools-src", "data", "spells")
+		start = time.Now()
+		files []string
+	)
 
 	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
@@ -27,72 +35,79 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var spells []json.RawMessage
 	for _, file := range files {
-		_, err = extractSpells(file)
+		logger := slog.With(slog.String("file", file))
+
+		if strings.Contains(file, "fluff") {
+			logger.InfoContext(ctx, "skipping fluff")
+			continue
+		}
+
+		if !strings.Contains(file, "spells-") {
+			logger.InfoContext(ctx, "skipping non spells file")
+			continue
+		}
+
+		res, err := extractRawSpellList(file)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		spells = append(spells, res...)
 	}
+
+	_, err = spellMarkdown(ctx, spells[0])
+
+	slog.InfoContext(ctx, "finished parsing files",
+		slog.Duration("elapsed", time.Since(start)),
+		slog.Group("spells",
+			slog.Int("count", len(spells)),
+		))
 }
 
-func extractSpells(path string) ([]Spell, error) {
+func spellMarkdown(ctx context.Context, m json.RawMessage) (*string, error) {
+	data, err := m.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	spell := map[string]any{}
+	err = json.Unmarshal(data, &spell)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal spell")
+	}
+
+	for field, value := range spell {
+		slog.InfoContext(ctx, "spell",
+			slog.String("field", field),
+			slog.Any("value", value),
+		)
+	}
+
+	return nil, nil
+}
+
+func extractRawSpellList(path string) ([]json.RawMessage, error) {
+	if len(path) == 0 {
+		return nil, fmt.Errorf("empty path")
+	}
+
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
-	var list Spells
+	var list struct {
+		Spell []json.RawMessage `json:"spell"`
+	}
 	err = json.Unmarshal(bytes, &list)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal json for file %s with error: %w", path, err)
 	}
 
+	if len(list.Spell) == 0 {
+		return nil, fmt.Errorf("no spells found in %s", path)
+	}
+
 	return list.Spell, nil
-}
-
-type Spells struct {
-	Spell []Spell `json:"spell"`
-}
-
-type Spell struct {
-	Name        string   `json:"name"`
-	Source      string   `json:"source"`
-	Page        int      `json:"page"`
-	Srd         any      `json:"srd"`
-	BasicRules  bool     `json:"basicRules"`
-	ReprintedAs []string `json:"reprintedAs"`
-	Level       int      `json:"level"`
-	School      string   `json:"school"`
-	Time        []struct {
-		Number int    `json:"number"`
-		Unit   string `json:"unit"`
-	} `json:"time"`
-	Range struct {
-		Type     string `json:"type"`
-		Distance struct {
-			Type   string `json:"type"`
-			Amount int    `json:"amount"`
-		} `json:"distance"`
-	} `json:"range"`
-	Components struct {
-		V bool `json:"v"`
-		S bool `json:"s"`
-	} `json:"components"`
-	Duration []struct {
-		Type string `json:"type"`
-	} `json:"duration"`
-	Entries          []string `json:"entries"`
-	ScalingLevelDice struct {
-		Label   string `json:"label"`
-		Scaling struct {
-			Field1 string `json:"1"`
-			Field2 string `json:"5"`
-			Field3 string `json:"11"`
-			Field4 string `json:"17"`
-		} `json:"scaling"`
-	} `json:"scalingLevelDice"`
-	DamageInflict []string `json:"damageInflict"`
-	SavingThrow   []string `json:"savingThrow"`
-	MiscTags      []string `json:"miscTags"`
-	AreaTags      []string `json:"areaTags"`
 }
